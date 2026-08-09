48ab8719992ec898d0d355fb76c5faf68ff8dc98
Atharv Jadhav <jadhavatharv3104@gmail.com>
2026-08-10 00:41:36 +0530

    canvasEngine.js

--- canvasEngine.js
+++ canvasEngine.js
@@ -719,30 +719,47 @@ class CanvasEngine {
     ctx.fillRect(qrX - 4, qrY - 4, qrSize + 8, qrSize + 8);
     ctx.drawImage(qrCanvas, qrX, qrY, qrSize, qrSize);
 
- // 4. "2:47PM STUDIO" Graphic Sticker Image at Left Side of QR Code
-const stickerX = W / 2 - 200;
-const stickerY = 870;
-const stickerW = 150;
-const stickerH = 75;
+    // 4. "2:47PM STUDIO" Graphic Sticker Image at Left Side of QR Code
+    const stickerX = W / 2 - 200;
+    const stickerY = 870;
+    const stickerW = 150;
+    const stickerH = 75;
 
-await new Promise((resolve) => {
-  const stImg = new Image();
-  stImg.onload = () => {
-    ctx.save();
-    ctx.shadowColor = "rgba(0, 0, 0, 0.6)";
-    ctx.shadowBlur = 8;
-    ctx.drawImage(stImg, stickerX, stickerY, stickerW, stickerH);
+    await new Promise((resolve) => {
+      const stImg = new Image();
+      stImg.onload = () => {
+        ctx.save();
+        ctx.shadowColor = "rgba(0, 0, 0, 0.6)";
+        ctx.shadowBlur = 8;
+        ctx.drawImage(stImg, stickerX, stickerY, stickerW, stickerH);
+        ctx.restore();
+        resolve();
+      };
+      stImg.onerror = () => {
+        resolve();
+      };
+      stImg.src = "studio_sticker.png";
+    });
+
     ctx.restore();
-    resolve();
-  };
-  stImg.onerror = () => {
-    resolve();
-  };
-  stImg.src = "/studio_sticker.png";
-});
+  }
 
-ctx.restore();
-}
+  static drawProceduralBarcode(ctx, x, y, w, h) {
+    ctx.save();
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(x, y, w, h);
+
+    ctx.fillStyle = "#000000";
+    let curX = x + 10;
+    const barWidths = [2, 1, 3, 1, 2, 4, 1, 2, 1, 3, 2, 1, 4, 1, 2, 1, 3, 2, 1, 4];
+    
+    barWidths.forEach(bw => {
+      ctx.fillRect(curX, y + 4, bw, h - 8);
+      curX += bw + 2;
+    });
+
+    ctx.restore();
+  }
 
   static drawOfficialFooterLinks(ctx, W, H) {
     ctx.save();
